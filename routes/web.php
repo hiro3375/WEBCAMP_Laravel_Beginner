68ed86e03045aa703099876b8586c8606fe89c9c
ひろ <hiro@example.com>
2026-06-28 04:47:26 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
